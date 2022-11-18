360598ec6e463b821edf6d35aa780f9ae747b092
Ifiok Ekott <ekottifiok@gmail.com>
2022-11-18 04:57:38 +0100

    added a readme file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a binary node
+ * 
+ * @param parent 
+ * @param value 
+ * @return binary_tree_t* 
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *newTree;
